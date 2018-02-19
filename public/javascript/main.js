6622f4d4e5b3b3140a3bc2d33fa99298a61900f4
Devin Cenatiempo <dcenatiempo@gmail.com>
2018-02-19 10:25:47 -0700

    working out the card system

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -40,21 +40,35 @@ function saveLocal(json){
 
 
 function getWeather(myLocal){
-  var prodUrl = 'https://devins-weather-app.herokuapp.com/';
-  var devUrl = 'http://localhost:5000/';
-  var path = 'weather/'
-  var url = devUrl + path + myLocal.lat + ',' + myLocal.long;
-  var request = new Request(url, {
-      mode: 'no-cors'
-    });
+//   var prodUrl = 'https://devins-weather-app.herokuapp.com/';
+//   var devUrl = 'http://localhost:5000/';
+//   var path = 'weather/'
+//   var url = devUrl + path + myLocal.lat + ',' + myLocal.long;
+//   var request = new Request(url, {
+//       mode: 'no-cors'
+//     });
+// //TODO: figure out CORS settings for production
+//   fetch(request)
+//   .then(resp => {
+//     console.log(resp);
+//     return resp.json();
+//   }).then(json => {
+//     console.log (json)
+//     weather = json;
+//   }).catch(err => {
+//     console.error(err);
+//   });
+}
 
-  fetch(request)
-  .then(resp => {
-    return resp.json();
-  }).then(json => {
-    console.log (json)
-    weather = json;
-  }).catch(err => {
-    console.error(err);
-  });
+var weatherCards = document.querySelectorAll('.city-page')
+for (let i=0; i<weatherCards.length; i++){
+
+  weatherCards[i].addEventListener('click', e => {
+    console.log(i + " clicky!");
+    weatherCards[i].classList.toggle('back');
+    weatherCards[i].classList.toggle('front');
+    weatherCards[Math.abs(i-1)].classList.toggle('back');
+    weatherCards[Math.abs(i-1)].classList.toggle('front');
+  })
+
 }
