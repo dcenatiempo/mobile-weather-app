8d9e368ec7a39d7e866fadf53e962d9600ab7e11
Devin Cenatiempo <devincenatiempo@familysearch.org>
2018-02-19 12:56:27 -0700

    implemented card rotation with arrow keys

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -5,6 +5,20 @@
 console.log("hello world")
 var myLocal = {};
 var weather = {};
+var cards = {
+  a: {
+    rotation: 0,
+    position: 0 //'front'
+  },
+  b: {
+    rotation: 180,
+    position: 1 //'right'
+  },
+  c: {
+    rotation: -180,
+    position: 2 //'left'
+  }
+}
 
 fetch('https://freegeoip.net/json/?callback=')
   .then(resp => {
@@ -61,14 +75,91 @@ function getWeather(myLocal){
 }
 
 var weatherCards = document.querySelectorAll('.city-page')
-for (let i=0; i<weatherCards.length; i++){
 
-  weatherCards[i].addEventListener('click', e => {
-    console.log(i + " clicky!");
-    weatherCards[i].classList.toggle('back');
-    weatherCards[i].classList.toggle('front');
-    weatherCards[Math.abs(i-1)].classList.toggle('back');
-    weatherCards[Math.abs(i-1)].classList.toggle('front');
+
+  window.addEventListener('keydown', e => {
+    if (e.keyCode === 37) { // rotate left
+      console.log('rotate left')
+      cards = rotateLeft(cards);
+      updateCardsInDOM(cards)
+    }
+    else if (e.keyCode === 39) { // rotate right
+      console.log('rotate right')
+      cards = rotateRight(cards)
+      updateCardsInDOM(cards)
+    }
   })
 
+function rotateLeft (cards) {
+  // console.log(cards)
+  return {
+    a: {
+      rotation: normalizePos(cards.a.position) === 2 ? cards.a.rotation : cards.a.rotation -180,
+      position: cards.a.position - 1
+    },
+    b: {
+      rotation: normalizePos(cards.b.position) === 2 ? cards.b.rotation : cards.b.rotation -180,
+      position: cards.b.position - 1
+    },
+    c: {
+      rotation: normalizePos(cards.c.position) === 2 ? cards.c.rotation : cards.c.rotation -180,
+      position: cards.c.position - 1
+    }
+  };
+}
+
+function rotateRight (cards) {
+  // console.log(cards)
+  return {
+    a: {
+      rotation: normalizePos(cards.a.position) === 1 ? cards.a.rotation : cards.a.rotation +180,
+      position: cards.a.position + 1
+    },
+    b: {
+      rotation: normalizePos(cards.b.position) === 1 ? cards.b.rotation : cards.b.rotation +180,
+      position: cards.b.position + 1
+    },
+    c: {
+      rotation: normalizePos(cards.c.position)=== 1 ? cards.c.rotation : cards.c.rotation +180,
+      position: cards.c.position + 1
+    }
+  };
+}
+
+function updateCardsInDOM (c) {
+  var card;
+  for (let key in c) {
+    // console.log(key)
+    card = document.querySelector(`#${key}`);
+    card.classList.remove('left', 'right', 'front');
+    card.classList.add(`${numToPos(c[key].position)}`);
+    console.log(c[key].rotation)
+    card.style.transform = `rotateY(${c[key].rotation}deg)`
+  }
+}
+
+function normalizePos(num) {
+  var mod = num%3;
+  if (mod < 0) {
+    while (mod <0){
+      mod = mod+3
+    }
+  }
+  return mod;
+}
+function numToPos (num) {
+
+  var pos;
+  switch (normalizePos(num)) {
+    case 0: 
+      pos = 'front';
+      break;
+    case 1:
+      pos = 'right';
+      break;
+    case 2:
+      pos = 'left';
+      break;
+  }
+  return pos;
 }
